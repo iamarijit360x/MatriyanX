37831d4732f3f89c8db9f4fd82884fb63c977597
Arijit Banerjee <arijit.bandyos7@gmail.com>
2024-10-06 02:47:34 +0530

    Added CORS

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from .routes import register_routes
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    app.url_map.strict_slashes = False  # Disable redirect on missing slash
+    # Allow CORS for all routes from specific origins
+    CORS(app, origins=["http://localhost:3000"], supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
 
-    #SAMPLE TEST
+    # Sample test route
     @app.route('/')
-    def method_name():
+    def welcome():
         return 'Welcome to MatrianX API'
     
+    # Register additional routes
     register_routes(app)
 
     return app
