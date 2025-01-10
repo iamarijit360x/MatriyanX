53ae01c2826d6940dec9010b2574ccacc9f94923
Arijit Banerjee <arijit.bandyos7@gmail.com>
2025-01-10 22:27:52 +0530

    Update CORS configuration to use environment variable for front-end URL

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from .routes import register_routes
 from flask_cors import CORS
-
+import os
 def create_app():
     app = Flask(__name__)
     app.url_map.strict_slashes = False  # Disable redirect on missing slash
     # Allow CORS for all routes from specific origins
-    CORS(app, origins=["http://localhost:3000"], supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
+    CORS(app, origins=[os.getenv('FRONT_END_URL')], supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
 
     # Sample test route
     @app.route('/')
